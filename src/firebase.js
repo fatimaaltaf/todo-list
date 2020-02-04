70ed2724c5ee51f2d7c59c4076ccd6ee4a956c88
Fatima Altaf <fatima.altaf@gmail.com>
2020-02-04 14:41:21 -0500

    firebase credentials add to app

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app';
-import 'firebase/firestore';
+import 'firebase/firestore'; 
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagerSendersId: '',
-  apiId: ''
+  apiKey: "AIzaSyDkMUIerbfyqugPL2I3HSu9fZsk907WWtE",
+  authDomain: "to-do-list-a8be1.firebaseapp.com",
+  databaseURL: "https://to-do-list-a8be1.firebaseio.com",
+  projectId: "to-do-list-a8be1",
+  storageBucket: "to-do-list-a8be1.appspot.com",
+  messagingSenderId: "1055763226826",
+  appId: "1:1055763226826:web:2ddf9db54d268239f55433"
 }) // Pass object into it and API Key
 
 export { firebaseConfig as firebase };
